c3b5206481f7356f7990b6f374ac9ab7a59abd59
wooof4 <tytus8392@gmail.com>
2026-09-06 04:27:47 +0000

    Sep 6, 2026, 4:27 AM

--- testquestion2.js
+++ testquestion2.js
@@ -1,3 +1,17 @@
+let ball;
 function setup(){
-    createCanva(600,400)
+    createCanvas(600,400);
+    background('white');
+    world.gravity.y=5;
+}
+function draw(){
+    background('white')
+    if (mouse.presses()){
+    ball=new Sprite();
+    ball.diameter=50;
+    ball.x=mouse.x;
+    ball.y=mouse.y;
+    ball.collider='dynamic';  
+    ball.mass=1;
+    }
 }
