7a708ec01bf1696477c10c52849c6d71e72bc094
wooof4 <tytus8392@gmail.com>
2026-09-06 04:30:29 +0000

    Sep 6, 2026, 4:30 AM

--- testquestion2.js
+++ testquestion2.js
@@ -18,6 +18,6 @@ function draw(){
     floor.width=600;
     floor.height=20;
     floor.x=width/2;
-    floor.y=height-20
-    floor.collider='static'
+    floor.y=height-20;
+    floor.collider='static';
     }
