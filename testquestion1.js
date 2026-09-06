8cc70ed55c7a74a93f82131ace6f15488f0dc187
wooof4 <tytus8392@gmail.com>
2026-09-06 04:10:24 +0000

    Sep 6, 2026, 4:10 AM

--- testquestion1.js
+++ testquestion1.js
@@ -12,7 +12,8 @@ function draw(){
 function mousePressed(){
         ball=new Sprite();
         ball.x=mouse.x;
-        ball.y=mouse.y;    ball.collider='dynamic';  
+        ball.y=mouse.y;
+    ball.collider='dynamic';  
     ball.mass=2;
     ball.gravity=192.6;
     ball.diameter=50; 
